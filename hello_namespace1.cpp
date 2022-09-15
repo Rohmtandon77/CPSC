616f578708c11a02de194bd86d769586fdf6e50c
Rohm Tandon <92406109+Rohmtandon77@users.noreply.github.com>
2022-09-14 20:31:52 -0700

    Add files via upload

---
+++ hello_namespace1.cpp
@@ -0,0 +1,11 @@
+//
+/// @file <hello.cpp>
+/// @brief <A file that outputs Hello C++>
+/// @author <Rohm Tandon>
+
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "Hello C++!" << endl;
+    return 0;
+}
